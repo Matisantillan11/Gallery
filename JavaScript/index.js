fa98692fd9e4d75bac61a92e9114822663c75122
unknown <matias1.santillan@gmail.com>
2020-08-07 03:17:21 -0300

    popup for presentation added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,6 +4,8 @@ const conteinerImage = document.querySelector(".conteiner-img");
 const showImage = document.querySelector(".show")
 const text = document.querySelector(".description")
 const close = document.querySelector(".bx-x");
+const conteinerMessage = document.querySelector(".message-conteiner");
+const close2 = document.querySelector(".dos"); 
 
 images.forEach(image => {
     var src = image.getAttribute("src");
@@ -16,6 +18,10 @@ images.forEach(image => {
 
 close.addEventListener("click", ()=>{
     conteinerImage.classList.toggle("move");
+});
+
+close2.addEventListener("click", ()=>{
+    conteinerMessage.classList.toggle("move2");
 });
 
 const addImage = (srcImage, altImage)=>{
